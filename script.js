4768c7a74f78808364bebb4d589bd3b0e489e3c5
digitarod <digitarod@gmail.com>
2025-12-14 12:34:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwizUZGKx2LUv0G4bWJRJcIlWdHt3DjD40YrpNyp7-CZ4CDQtZ422bX_fWO1CHCAwy2Zg/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbx4ZLpBtf3-gjDJb9QxY_ODTAo945hYDiEjjDISabBSOksc2Q6LOZDPTXa7ux6i-ZD4hA/exec';
 
 // 状態管理
 let currentUser = null;
